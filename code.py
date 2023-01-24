24683cb13f87a7b9dea1b706745628c24b21398c
H Y S T E R 1 A <97292612+Hyster1a20@users.noreply.github.com>
2023-01-24 07:55:03 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,14 @@
+import translate 
+from translate import Translator
 
+#base + res (variables) 
+#translator azbi rah mne lang to lang 
+#from_langage , to_langage
+
+base = Translator(from_lang="English",to_lang="Spanish")
+
+res = base.translate("hello") 
+
+#u can change the word u want to translate in the fx base.translate() 
+
+print(res)
